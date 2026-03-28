7a20c91b4725bb06ecf00cd8543a214483bcc67c
Adrian55Stack <adrianciurea112@yahoo.com>
2026-03-28 21:54:34 +0200

    first commit

---
+++ personal-dashboard-be/src/test/java/com/example/personaldashboardbe/PersonalDashboardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personaldashboardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDashboardBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
