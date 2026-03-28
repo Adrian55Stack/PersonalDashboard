31db8111da044e6cb472d2c7599cfd60ef8fb8e5
Adrian55Stack <adrianciurea112@yahoo.com>
2026-03-28 22:10:03 +0200

    add migrations

---
+++ personal-dashboard-be/src/main/resources/db/migration/V1__create_table_notes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE notes (
+    id INT PRIMARY KEY,
+    title VARCHAR(50),
+    content VARCHAR(200),
+    created_at DATETIME
+)
---
+++ personal-dashboard-be/src/main/resources/db/migration/V2__create_table_links.sql
@@ -0,0 +1,6 @@
+CREATE TABLE links (
+    id INT PRIMARY KEY,
+    url VARCHAR(50),
+    description VARCHAR(200),
+    created_at DATETIME
+)
