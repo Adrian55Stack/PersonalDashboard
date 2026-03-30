a2d2123d15da0350cdc866ca8276f6e9c6d5c1a2
Adrian55Stack <adrianciurea112@yahoo.com>
2026-03-30 21:44:16 +0300

    allow FE origin

---
+++ personal-dashboard-be/src/main/java/com/example/personaldashboardbe/configurers/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.personaldashboardbe.configurers;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+};
